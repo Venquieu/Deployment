e60f61237a86506fdde4689e10b1766e994eaece
Venquieu <Richard_wj237@outlook.com>
2024-01-23 09:27:07 +0800

    feat: better inference pipeline

--- onnx2trt/trt_inference.py
+++ onnx2trt/trt_inference.py
@@ -10,20 +10,21 @@ cudart.cudaDeviceSynchronize()
 
 def make_parser():
     parser = argparse.ArgumentParser("TensorRT engine inference")
-    parser.add_argument("--path", required=True, help="image path for inference")
+    parser.add_argument("--engine", required=True, help="path of TensorRT engine")
+    parser.add_argument("--img", required=True, help="image path for inference")
     parser.add_argument(
         "--shape", required=True, help="model input shape with format `C,H,W`"
     )
     return parser
 
 
-class NetRT(object):
+class NetRuntime(object):
     def __init__(self, engine_path, input_shape) -> None:
         logger = trt.Logger(trt.Logger.VERBOSE)
 
         with open(engine_path, "rb") as f:
-            engineString = f.read()
-        engine = trt.Runtime(logger).deserialize_cuda_engine(engineString)
+            engine_string = f.read()
+        engine = trt.Runtime(logger).deserialize_cuda_engine(engine_string)
 
         num_io = engine.num_io_tensors
         tensor_names = [engine.get_tensor_name(i) for i in range(num_io)]
@@ -34,73 +35,81 @@ class NetRT(object):
         context = engine.create_execution_context()
         context.set_input_shape(tensor_names[0], [1, *input_shape])
 
-        for i in range(num_io):
-            print(
-                "[%2d]%s->" % (i, "Input " if i < num_input else "Output"),
-                engine.get_tensor_dtype(tensor_names[i]),
-                engine.get_tensor_shape(tensor_names[i]),
-                context.get_tensor_shape(tensor_names[i]),
-                tensor_names[i],
-            )
-
         self.num_input = num_input
         self.num_io = num_io
         self.tensor_names = tensor_names
         self.context = context
         self.engine = engine
 
-    def execute(self, img):
-        buffer_host = []
-        buffer_host.append(np.ascontiguousarray(img))
+        self.alloc_memory(input_shape)
+
+        print("I/O tensor info:")
+        self.print_io_info()
+
+    def alloc_memory(self, input_shape):
+        # alloc host memory
+        self.buffer_host = []
+        self.buffer_host.append(np.empty((1, *input_shape), dtype=np.float32))
         for i in range(self.num_input, self.num_io):
             out_shape = self.context.get_tensor_shape(self.tensor_names[i])
             out_dtype = self.engine.get_tensor_dtype(self.tensor_names[i])
-            buffer_host.append(np.empty(out_shape, dtype=trt.nptype(out_dtype)))
+            self.buffer_host.append(np.empty(out_shape, dtype=trt.nptype(out_dtype)))
 
-        buffer_device = []
+        # alloc device memory
+        self.buffer_device = []
         for i in range(self.num_io):
-            buffer_device.append(cudart.cudaMalloc(buffer_host[i].nbytes)[1])
+            self.buffer_device.append(cudart.cudaMalloc(self.buffer_host[i].nbytes)[1])
 
+        # binding tensor and memory
+        for i in range(self.num_io):
+            self.context.set_tensor_address(self.tensor_names[i], int(self.buffer_device[i]))
+
+    def execute(self, img):
         for i in range(self.num_input):
+            self.buffer_host[i] = img
             cudart.cudaMemcpy(
-                buffer_device[i],
-                buffer_host[i].ctypes.data,
-                buffer_host[i].nbytes,
+                self.buffer_device[i],
+                self.buffer_host[i].ctypes.data,
+                self.buffer_host[i].nbytes,
                 cudart.cudaMemcpyKind.cudaMemcpyHostToDevice,
             )
-
-        for i in range(self.num_io):
-            self.context.set_tensor_address(self.tensor_names[i], int(buffer_device[i]))
 
         self.context.execute_async_v3(0)
 
         for i in range(self.num_input, self.num_io):
             cudart.cudaMemcpy(
-                buffer_host[i].ctypes.data,
-                buffer_device[i],
-                buffer_host[i].nbytes,
+                self.buffer_host[i].ctypes.data,
+                self.buffer_device[i],
+                self.buffer_host[i].nbytes,
                 cudart.cudaMemcpyKind.cudaMemcpyDeviceToHost,
             )
 
+    def print_io_info(self):
         for i in range(self.num_io):
-            print(self.tensor_names[i])
-            print(buffer_host[i])
-
-        for b in buffer_device:
-            cudart.cudaFree(b)
+            print(
+                "[%2d]%s->" % (i, "Input " if i < self.num_input else "Output"),
+                self.engine.get_tensor_dtype(self.tensor_names[i]),
+                self.engine.get_tensor_shape(self.tensor_names[i]),
+                self.context.get_tensor_shape(self.tensor_names[i]),
+                self.tensor_names[i],
+            )
 
     def __del__(self):
-        pass
+        for b in self.buffer_device:
+            cudart.cudaFree(b)
 
 
 if __name__ == "__main__":
     args = make_parser().parse_args()
     input_shape = [int(x) for x in args.shape.split(",")]
 
+    net = NetRuntime(args.engine, input_shape)
     data = (
-        cv2.imread(args.path, cv2.IMREAD_GRAYSCALE)
+        cv2.imread(args.img, cv2.IMREAD_GRAYSCALE)
         .astype(np.float32)
         .reshape(1, *input_shape)
     )
 
+    net.execute(data)
+    result = net.buffer_device[net.num_input:]
     print("Succeeded running model in TensorRT!")
